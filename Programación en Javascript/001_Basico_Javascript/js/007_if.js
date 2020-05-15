f2d9449072139c38c6eb629721409476ed8b41f7
lfzarazuaa <lfzarazuaa1400@outlook.com>
2020-05-15 12:15:54 -0500

    If else en JS

---
+++ Programación en Javascript/001_Basico_Javascript/js/007_if.js
@@ -0,0 +1,15 @@
+var edad = 18;
+
+if (edad === 18) {
+    console.log("Puedes votar, será tu primera votación.");
+} else if(edad>18){
+    console.log("Puedes votar de nuevo");
+}
+else {
+    console.log("Aun no puedes votar");
+}
+
+// Operador Condicional 
+var numero = 1;
+
+var resultado = numero === 1 ? "Si soy un uno" : "No soy un uno";
