1350733b83cc490d3c15281a4a6877a66997779d
lfzarazuaa <lfzarazuaa1400@outlook.com>
2020-05-15 12:23:41 -0500

    Switch case en JS.

---
+++ Programación en Javascript/001_Basico_Javascript/js/008_switch.js
@@ -0,0 +1,19 @@
+var numero = 10;
+
+switch (numero) {
+    case 1:
+        console.log("¡Soy un uno!")
+        break;
+    case 10:
+        console.log("¡Soy un diez!");
+        break;
+    case 100:
+        console.log("¡Soy un cien!");
+        break;
+    case 1000:
+        console.log("¡Soy un mil!");
+        break;
+    default:
+        console.log("¡Soy otro número!");
+        break;
+}
