8a41b8093e335faddc3124f393f4fe00db40b4ac
lfzarazuaa <lfzarazuaa1400@outlook.com>
2020-05-15 00:37:18 -0500

    Agregadas Notas sobre funciones en Javascript.

---
+++ Programación en Javascript/001_Basico_Javascript/js/003_Funciones.js
@@ -0,0 +1,14 @@
+//Función declarativa
+function miFuncion() {
+    return 3;
+}
+
+//Llamada a función.
+miFuncion()
+
+//Función expresiva o anónima
+var miFuncion = function(a,b){
+    return a + b;
+}
+
+miFuncion(2,5)//Asigna en la variable mi función lo que regresa function.
