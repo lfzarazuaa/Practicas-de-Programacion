4a66504ca68fe68f7281762267ea5051e2f0f5ab
lfzarazuaa <lfzarazuaa1400@outlook.com>
2020-05-15 11:22:48 -0500

    Coerción en JS.

---
+++ Programación en Javascript/001_Basico_Javascript/js/006_Coercion.js
@@ -0,0 +1,7 @@
+var a = 4 + "7";//Coerción implicita resultante en String.
+
+var b = 4 * "7";//Coerción implicita resultante en Number.
+
+var c = String(4);//Coerción explicita resultante en String.
+
+var d = Number(c);//Coerción explicita resultante en Number.
