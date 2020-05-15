11f6512841aa1556def42d8ffd15b9178e720fc6
lfzarazuaa <lfzarazuaa1400@outlook.com>
2020-05-15 00:17:05 -0500

    Conceptos de valores y variables en Js.

---
+++ Programación en Javascript/001_Basico_Javascript/js/001_valores.js
@@ -0,0 +1,8 @@
+40 //Valor entero
+"Luis Fernando Zarazua"//Valor String
+true//Valores booleanos.
+false 
+null//valores place holder
+undefined
+
+[1,2,3]//Tipo arreglo
---
+++ Programación en Javascript/001_Basico_Javascript/js/002_variables.js
@@ -0,0 +1,16 @@
+var nombre = Oscar;
+
+//Declarar variables
+var edad;
+
+//Inicializar variable
+edad = 30;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+typeof persona //Devuelve object.actions
