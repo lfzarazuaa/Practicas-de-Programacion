5f646c276f6cebd9b8a037a615560f9ad893e979
lfzarazuaa <lfzarazuaa1400@outlook.com>
2020-05-17 12:54:33 -0500

    Variables numéricas en JS.

---
+++ Programación en Javascript/002_Fundamentos_JavaScript/003_variables_numericas/codigo.js
@@ -0,0 +1,13 @@
+var edad = 27;
+//edad = edad +1
+edad+=1;
+
+var peso =75
+peso -= 2;
+
+var sandwich = 1;
+peso= peso + sandwich;
+
+precioDeBebida = 200.3;
+
+var total = Math.round(precioDeBebida * 100 * 3) / 100;//JavaScript no es muy preciso con los números por defecto.
