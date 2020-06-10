8aea0fddf9376b01d96adbce89da9c31d174db23
lfzarazuaa <lfzarazuaa1400@outlook.com>
2020-06-10 18:05:47 -0500

    Clases en JavaScript y PHP.

---
+++ Programacion_Orientada_a_Objetos/CursoPOOUber/JS/index.js
@@ -0,0 +1,2 @@
+var name=Luis;
+    console.log(name);
---
+++ Programacion_Orientada_a_Objetos/CursoPOOUber/PHP/account.php
@@ -0,0 +1,11 @@
+<?php
+class Account
+{
+    public $id;
+    public $name;
+    public $document;
+    public $email;
+    public $password;
+}
+    
+?>
