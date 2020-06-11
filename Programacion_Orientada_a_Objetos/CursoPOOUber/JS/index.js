a5b7898e6944b2b3f25a72603ab374110abf72c0
lfzarazuaa <lfzarazuaa1400@outlook.com>
2020-06-11 00:46:13 -0500

    Metodo constructor en JavaScript.

--- Programacion_Orientada_a_Objetos/CursoPOOUber/JS/index.js
+++ Programacion_Orientada_a_Objetos/CursoPOOUber/JS/index.js
@@ -1,2 +1,3 @@
-var name=Luis;
-    console.log(name);
+var car = new Car("aw456", new Account("Andres Herrera", "AND123"));
+car.passenger = 4;
+car.printDataCar();
--- Programacion_Orientada_a_Objetos/CursoPOOUber/Java/Main.java
+++ Programacion_Orientada_a_Objetos/CursoPOOUber/Java/Main.java
@@ -1,13 +1,9 @@
 class Main{
     public static void main(String[] args) {
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
         car.passenger = 4;
         
-        Car car2 = new Car();
-        car2.license = "QWE567";
-        car2.driver = "Gim Martinez";
+        Car car2 = new Car("QWE567", new Account("Gim Martinez", "GIM123"));
         car2.passenger = 3;
 
         car.printDataCar();
