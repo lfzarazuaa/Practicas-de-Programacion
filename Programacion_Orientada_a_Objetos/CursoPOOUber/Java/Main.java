9062a44530f51049ba48883ac5a983eaac1c0db7
lfzarazuaa <lfzarazuaa1400@outlook.com>
2020-06-10 23:59:14 -0500

    Creacion de objetos en Java y Python.

--- Programacion_Orientada_a_Objetos/CursoPOOUber/Java/Main.java
+++ Programacion_Orientada_a_Objetos/CursoPOOUber/Java/Main.java
@@ -1,5 +1,16 @@
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andres Herrera";
+        car.passenger = 4;
+        
+        Car car2 = new Car();
+        car2.license = "QWE567";
+        car2.driver = "Gim Martinez";
+        car2.passenger = 3;
+
+        car.printDataCar();
+        car2.printDataCar();
     }
 }
--- Programacion_Orientada_a_Objetos/CursoPOOUber/Python/main.py
+++ Programacion_Orientada_a_Objetos/CursoPOOUber/Python/main.py
@@ -1,2 +1,12 @@
+from car import Car
+
 if __name__ == "__main__":
-    print("Hola mundo")
+    car = Car()
+    car.license = "AMS234"
+    car.driver = "Andres Herrera"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "QWE567"
+    car2.driver = "Gim Martinez"
+    print(vars(car2))
