919c66e5f39ff8d6a148c998a250b1c2920ea7e8
lfzarazuaa <lfzarazuaa1400@outlook.com>
2020-07-28 15:09:37 -0500

    Curso Sql Azure Finalizado.

--- Azure/Sql_Azure/consultaBasica2.sql
+++ Azure/Sql_Azure/consultaBasica2.sql
@@ -1,4 +1,4 @@
 INSERT INTO SalesLT.ProductCategory (Name, rowguid, ModifiedDate)
-VALUES ('Boxers', 'cfbda25c-df71-47a7-b81b-64ee161aa37c', 2020-06-01)
+VALUES ('Boxers', '8fbaa25c-df71-47a7-b81b-64ee161aa37c', 2020-06-01)
 
 SELECT * FROM SalesLT.ProductCategory
